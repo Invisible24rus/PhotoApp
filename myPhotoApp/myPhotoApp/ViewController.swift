c101d082249b6581bc8e7edf0b764b5605e703ae
Nikita <invisible24rus@gmail.com>
2022-03-22 08:17:50 +0300

    create

---
+++ myPhotoApp/myPhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myPhotoApp
+//
+//  Created by NIKITA NIKOLICH on 22.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+dsadsad
+}
+
